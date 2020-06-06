96f12b5911130490216400512ad1bdc67aa0512f
joaofelipe <ncrawlermaiden@gmail.com>
2020-06-06 01:51:47 -0300

    finalizado backend projeto

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,18 @@
 import express from 'express';
 
-const routes = express();
+const routes = express.Router();
 
-routes.get('/', (req, res) =>{
-    res.json([ 'João Felipe', 'Aline', 'Rita', 'Ilcea']);
-});
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
+const pointController = new PointsController();
+const itemController = new ItemsController();
+
+
+// index, show, create, update, delete
+routes.get('/items', itemController.index)
+routes.get('/points/:id', pointController.show)
+routes.get('/points', pointController.index);
+routes.post('/points', pointController.create)
 
 export default routes;
