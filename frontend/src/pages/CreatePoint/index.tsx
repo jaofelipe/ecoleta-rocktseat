d7b9774484428bbd35ba80c5bd4eb6f6a83080a3
joaofelipe <ncrawlermaiden@gmail.com>
2020-06-07 23:57:06 -0300

    Adicionado pagina inicial do ponto de coleta e rota para criaçao de nova

---
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CreatePoint.css'
+
+
+
+const CreatePoint: React.FC = ()  =>{
+    return (
+       <h1>Hello planet</h1>
+
+    );
+}
+export default CreatePoint;
